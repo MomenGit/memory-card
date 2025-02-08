55c1e665d13f5d9a23d77d864acbf648ebb4a254
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2025-02-08 22:36:39 +0200

    Fix theme.extend closing braces bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       backgroundImage: {
         "background-img": 'url("/background.jpeg")',
       },
-    },
-    colors: {
-      primary: "#014A8E",
-      secondary: "#FDC708",
-    },
-    fontFamily: {
-      sans: ["fredoka", "ui-sans-serif", "system-ui"],
-      serif: ["ui-serif", "Georgia"],
-      mono: ['"Roboto Mono"', "ui-monospace", "SFMono-Regular"],
+      colors: {
+        primary: "#014A8E",
+        secondary: "#FDC708",
+      },
+      fontFamily: {
+        sans: ["fredoka", "ui-sans-serif", "system-ui"],
+        serif: ["ui-serif", "Georgia"],
+        mono: ['"Roboto Mono"', "ui-monospace", "SFMono-Regular"],
+      },
     },
   },
   plugins: [],
