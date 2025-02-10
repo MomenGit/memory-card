cd198667b7a0a1e746aefe1ca716efa2e42a856a
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2025-02-10 15:01:10 +0200

    fix: tone gameplay difficulty values

--- src/app/models/difficulty.type.ts
+++ src/app/models/difficulty.type.ts
@@ -1,7 +1,7 @@
 export type Difficulty = 'easy' | 'medium' | 'hard';
 
 export const difficultyCardsNoMap = {
-  easy: [8, 14],
-  medium: [12, 16],
+  easy: [8, 12],
+  medium: [12, 14],
   hard: [16, 18],
 };
--- src/app/models/level.type.ts
+++ src/app/models/level.type.ts
@@ -1,9 +1,9 @@
 export type Level = 1 | 2 | 3 | 4 | 5;
 
 export const levelTimeMap = {
-  1: 20,
-  2: 15,
-  3: 10,
-  4: 8,
-  5: 5,
+  1: 15,
+  2: 12,
+  3: 9,
+  4: 6,
+  5: 3,
 };
